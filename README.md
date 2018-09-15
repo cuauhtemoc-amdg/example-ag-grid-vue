d60b49b5c0566440d9496c12fdf31cd318a2211f
Adrian Hernandez <temoc@amdg-tech.com>
2018-09-14 18:37:39 -0600

    add base version of ag-Grid example

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # example-ag-grid-vue
-Example Using ag-Grid and VUE
+Example Using ag-Grid and VUE taken from 
+[ag-Grid Vue Example](https://www.ag-grid.com/vue-getting-started/)
+
+## install ag-Grid 
+Run this command to install ag-Grid:
+
+```
+npm install --save ag-grid-community ag-grid-vue
+
+```
+
+You may encounter issues like the following:
+
+```bash
+npm WARN babel-loader@8.0.2 requires a peer of @babel/core@^7.0.0 but none is installed. You must install peer dependencies yourself.
+
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@1.2.4 (node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@1.2.4: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+
++ ag-grid-community@19.0.0
++ ag-grid-vue@19.0.0
+updated 2 packages and audited 13862 packages in 67.976s
+found 0 vulnerabilities
+```
---
+++ my-project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="ag-theme-balham">
+    <ag-grid-vue style="width: 500px; height: 500px;"                 
+                 :columnDefs="columnDefs"
+                 :rowData="rowData">
+    </ag-grid-vue>
+  </div>
+</template>
+
+<script>
+    import {AgGridVue} from "ag-grid-vue";
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                columnDefs: null,
+                rowData: null
+            }
+        },
+        components: {
+            AgGridVue
+        },
+        beforeMount() {
+            this.columnDefs = [
+                {headerName: 'Make', field: 'make'},
+                {headerName: 'Model', field: 'model'},
+                {headerName: 'Price', field: 'price'}
+            ];
+
+            this.rowData = [
+                {make: 'Toyota', model: 'Celica', price: 35000},
+                {make: 'Ford', model: 'Mondeo', price: 32000},
+                {make: 'Porsche', model: 'Boxter', price: 72000}
+            ];
+        }
+    }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
